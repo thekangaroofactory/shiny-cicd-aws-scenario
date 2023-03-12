4d1d27337e3dcf41ba58f174535a7b42a48d5b81
kangarooaifr <philippe.peret@kangaroo-ai.fr>
2023-03-12 18:48:31 +0100

    Apply first change to test CI/CD

--- shinyapp/ui.R
+++ shinyapp/ui.R
@@ -11,23 +11,24 @@ library(shiny)
 
 # Define UI for application that draws a histogram
 fluidPage(
-
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-            plotOutput("distPlot")
-        )
+  
+  # Application title
+  titlePanel("My awesome titlePanel"),
+  
+  # Sidebar with a slider input for number of bins
+  sidebarLayout(
+    sidebarPanel(
+      sliderInput("bins",
+                  "Number of bins:",
+                  min = 1,
+                  max = 50,
+                  value = 30)
+    ),
+    
+    # Show a plot of the generated distribution
+    mainPanel(
+      h1("Trying CI/CD"),
+      plotOutput("distPlot")
     )
+  )
 )
